688766b76d1ba080cd2e07290125f9c4c53407dd
Brightmoon987 mezer <azzaretto.jan@iisgalvanimi.edu.it>
2023-12-17 14:46:54 +0000

    file

---
+++ esercitazione14_12/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'esercitazione14_12';
+
+  component_1 = "app"
+
+}
